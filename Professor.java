45bb71137e6f78cd6419f849384804d6eee19017
Hérica <hericadias987@gmail.com>
2025-02-27 15:35:59 -0300

    Professor.java

--- Professor.java
+++ Professor.java
@@ -1,17 +1,39 @@
-class Professor {
-    private String nome;
-    private boolean isCoordenador;
+public class Professor extends Pessoa {
 
-    public Professor(String nome, boolean isCoordenador) {
-        this.nome = nome;
-        this.isCoordenador = isCoordenador;
-    }
+  private String siape;
+ private boolean coordenador = false;
 
-    public String getNome() {
-        return nome;
-    }
+ public Professor(){
+ super();
+  }
+  
+  public Professor( String nome, String cpf, String telefone, String endereco, String siape){
+  super(nome, cpf, telefone, endereco);
+  this.siape = siape;
+  }
+ 
+ 
 
-    public boolean isCoordenador() {
-        return isCoordenador;
-    }
+  public String getSiape(){
+    return siape;
+  }
+
+  public void setSiape( String siape){
+    this.siape = siape;
+  }
+
+
+  public boolean getCoordenador(){
+    return coordenador;
+  }
+
+  public void setCoodernador( boolean coordenador){
+    this.coordenador = coordenador;
+  }
+
+  public void exibirDados(){
+    super.imprimirDados();
+    System.out.println("SIAPE: " + siape);
+  }
+
 }
