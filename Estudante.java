ed7e7237593c8b35e5d9ec8f42bf2b199cae9e82
Hérica <hericadias987@gmail.com>
2025-02-27 14:59:57 -0300

    Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -1,53 +1,39 @@
-class Estudante {
+public class Estudante extends Pessoa implements Printable {
     private String nome;
-    private double[] notas = new double[3];
-    private Double notaRecuperacao = null;
-
-    public Estudante(String nome) {
-        this.nome = nome;
+private String matricula;
+    
+    public Estudante(String Estutante) {
+        this.estudante = estutante;
     }
+public String getEstudante(){
+return  this.Estudante;
 
-    public String getNome() {
-        return nome;
-    }
 
-    public double[] getNotas() {
-        return notas;
-    }
+public void setEstudante(String Estudante) {
+this.Estundante =Estudante;
 
-    public Double getNotaRecuperacao() {
-        return notaRecuperacao;
+    public Matricula(String nome) {
+        this.matricula = matricula;
     }
 
-    public void setNotas(double[] notas) {
-        this.notas = notas;
-    }
 
-    public void setNotaRecuperacao(double nota) {
-        this.notaRecuperacao = nota;
-    }
+  public String getMatricula() {
+    return this.matricula;
+  }
 
-    public double calcularMediaSimples() {
-        return (notas[0] + notas[1] + notas[2]) / 3;
-    }
+  public void setMatricula(String matricula) {
+    this.matricula = matricula;
+  }
 
-    public double calcularMediaFinal() {
-        double media = calcularMediaSimples();
-        if (notaRecuperacao != null && media < 7) {
-            return (media + notaRecuperacao) / 2;
-        }
-        return media;
-    }
-    public String getStatus() {
-        double media = calcularMediaFinal();
-        if (notaRecuperacao == null && media < 7) {
-            return "SN";
-        } else if (media >= 7) {
-            return "APROVADO";
-        } else if (media >= 5) {
-            return "EM RECUPERAÇÃO";
-        } else {
-            return "REPROVADO";
-        }
-    }
+    public void exibirDados() {
+    System.out.println("Nome: " + this.getNome());
+    System.out.println("CPF: " + this.getCpf());
+    System.out.println("Endereço: " + this.getEndereco());
+    System.out.println("Telefone: " + this.getTelefone());
+    System.out.println("Matrícula: " + this.matricula);
+  }
+
+  public void exibirHistorico() {
+    System.out.println("Histórico do aluno " + this.getNome() + ":");
+  }
 }
