c9a6eefc8fe752121b3b0207a78d9df4cef63476
Hérica <hericadias987@gmail.com>
2025-02-27 16:31:03 -0300

    Turma.java

--- Turma.java
+++ Turma.java
@@ -1,14 +1,33 @@
 class Turma {
-    private String identificacao;
-    private String curso;
-    private Professor professor;
-    private List<Estudante> estudantes = new ArrayList<>();
+  import java.util.ArrayList;
+import java.util.List;
 
-    public Turma(String identificacao, String curso, Professor professor) {
+public class Turma {
+
+  private String identificacao;
+  private Curso curso;
+  private String semestre;
+  private ArrayList<Professor> professores = new ArrayList<>();
+  private ArrayList<Aluno> alunos = new ArrayList<>();
+  private List<Log> logs = new ArrayList<>();
+  Nota nota = new Nota();
+
+  public Turma(){
+
+  }
+
+    public Turma(String identificacao, String Curso curso, Professor professor) {
         this.identificacao = identificacao;
         this.curso = curso;
         this.professor = professor;
     }
+      public ArrayList<Professor> getProfessores() {
+    return professores;
+  }
+
+  public void setProfessores(ArrayList<Professor> professores) {
+    this.professores = professores;
+  }
 
     public void adicionarEstudante(Estudante estudante) {
         estudantes.add(estudante);
