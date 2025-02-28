8d6472cbba639ff9de42f30c56f6e034a84ccecc
Hérica <hericadias987@gmail.com>
2025-02-28 16:47:43 -0300

    Update Curso.java

---
+++ Curso.java
@@ -0,0 +1,39 @@
+public class Curso {
+
+    private String nome;
+    private int totalSemestres;
+    private Professor coordenador;
+    
+    public Curso() {
+    }
+    
+    public Curso(String nome, int totalSemestres) {
+        this.nome = nome;
+        this.totalSemestres = totalSemestres;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getTotalSemestres() {
+        return totalSemestres;
+    }
+    
+    public void setTotalSemestres(int totalSemestres) {
+        this.totalSemestres = totalSemestres;
+    }
+    
+    public void definirCoordenador(Professor professor) {
+        this.coordenador = professor;
+        System.out.println("Novo coordenador atribuído: " + professor.getNome());
+    }
+    
+    public Professor getCoordenador() {
+        return coordenador;
+    }
+}
