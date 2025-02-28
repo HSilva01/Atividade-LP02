2760ae899e2748940d7a9d7913af63a28d835605
Hérica <hericadias987@gmail.com>
2025-02-28 19:07:51 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -3,45 +3,48 @@ import java.util.List;
 
 public class Turma {
 
-  private String identificacao;
-  private Curso curso;
-  private String semestre;
-  private ArrayList<Professor> professores = new ArrayList<>();
-  private ArrayList<Estudante> estudantes = new ArrayList<>();
-  private List<Log> logs = new ArrayList<>();
-  Nota nota = new Nota();
-
-  public Turma(){
-
-  }
-
-    public Turma(String identificacao, Curso curso, Professor professor) {
-        this.identificacao = identificacao;
-        this.curso = curso;
-        this.array = professor;
+  
+    private String identificacao;
+    private Curso curso;
+    private String semestre;
+    private ArrayList<Professor> professores = new ArrayList<>();
+    private ArrayList<Estudante> estudantes = new ArrayList<>();
+    private List<Log> logs = new ArrayList<>();
+    Nota nota = new Nota();
+      
+    
+      public Turma(){
+    
+      }
+    
+        public Turma(String identificacao, Curso curso, String semestre) {
+            this.identificacao = identificacao;
+            this.curso = curso;
+            this.semestre = semestre;
+            this.logs = new ArrayList<>();
+      }
+        public ArrayList<Professor> getProfessores() {
+      return professores;
     }
-      public ArrayList<Professor> getProfessores() {
-    return professores;
-  }
-
-  public void setProfessores(ArrayList<Professor> professores) {
-    this.professores = professores;
-  }
-
-    public void adicionarEstudante(Estudante estudante) {
-        estudantes.add(estudante);
+  
+    public void setProfessores(ArrayList<Professor> professores) {
+      this.professores = professores;
     }
-
-    public List<Estudante> getEstudantes() {
-        return Arrayestudantes;
+  
+      public void adicionarEstudante(Estudante estudante) {
+          estudantes.add(estudante);
+      }
+  
+      public ArrayList<Estudante> getEstudantes() {
+          return estudantes;
     }
 
     public String getIdentificacao() {
         return identificacao;
     }
 
-    public Professor getProfessor() {
-        return Arrayprofessor;
+    public ArrayList<Professor> getProfessor() {
+        return professores;
     }
 }
 public class CalculadoraNotas {
@@ -53,31 +56,34 @@ public class CalculadoraNotas {
         return notas.length > 0 ? soma / notas.length : 0;
     }
     
-     String verificarSituacao(double media) {
-        if (media >= 7) {
-            return "Aprovado";
-        } else if (media >= 5) {
-            return "Em Recuperação";
-        } else {
-            return "Reprovado";
-        }
-    }
-    
-    public static void main(String[] args) {
-        double[] notas = {7.5, 8.0, 6.5};
-        double media = calcularMedia(notas);
-        System.out.println("Média: " + media);
-        System.out.println("Situação: " + verificarSituacao(media));
+     static String verificarSituacao(double media) {
+             if (media >= 7) {
+                 return "Aprovado";
+             } else if (media >= 5) {
+                 return "Em Recuperação";
+             } else {
+                 return "Reprovado";
+             }
+         }
+         
+         public static void main(String[] args) {
+             double[] notas = {7.5, 8.0, 6.5};
+             double media = calcularMedia(notas);
+             System.out.println("Média: " + media);
+             System.out.println("Situação: " + verificarSituacao(media));
     }
 }
 
 
 public void alterarNotaEstudante(Professor coordenador, Estudante estudante, double novaNota) {
     if (coordenador.getCoordenador()) {
-        double[] notasAnteriores = Nota.getNotas().clone();
+        double[] notasAnteriores = 
+          Nota.getNota1().clone();
+          Nota.getNota2().clone();
+          Nota.getNota3().clone();
         
         double[] novasNotas = {novaNota, novaNota, novaNota};
-        aluno.setNotas(novasNotas);
+        estudante.setNotas(novasNotas);
     } else {
         System.out.println("Somente coordenadores podem alterar notas");
     }
@@ -90,7 +96,7 @@ public void exibirDados(Turma turma) {
         professor.exibirDados();
     }
     System.out.println("Estutante:");
-    for (Estudante estudante : turma.getEstudante()) {
+    for (Estudante estudante : turma.getEstudantes()) {
         estudante.exibirDados();
     }
 }
