66c119363638918385777c6c965d9795d724fb4b
Hérica <hericadias987@gmail.com>
2025-03-01 01:45:09 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,36 +1,32 @@
 public class Professor extends Pessoa {
 
-  private String siape;
- private boolean coordenador = false;
+    private String siape;
+    private boolean coordenador = false;
 
+    public Professor(String nome, String cpf, String telefone, String endereco, String siape) {
+        super(nome, cpf, telefone, endereco);
+        this.siape = siape;
+    }
 
-  
-  public Professor( String nome, String cpf, String telefone, String endereco, String siape){
-  super(nome, cpf, telefone, endereco);
-  this.siape = siape;
-  }
- 
- 
+    public String getSiape() {
+        return siape;
+    }
 
-  public String getSiape(){
-    return siape;
-  }
+    public void setSiape(String siape) {
+        this.siape = siape;
+    }
 
-  public void setSiape( String siape){
-    this.siape = siape;
-  }
+    public boolean getCoordenador() {
+        return coordenador;
+    }
 
+    public void setCoodernador(boolean coordenador) {
+        this.coordenador = coordenador;
+    }
 
-  public boolean getCoordenador(){
-    return coordenador;
-  }
-
-  public void setCoodernador( boolean coordenador){
-    this.coordenador = coordenador;
-  }
-
-  public void exibirDados(){
-    System.out.println("SIAPE: " + siape);
-  }
+    @Override
+    public void exibirDados() {
+        System.out.println("SIAPE: " + siape);
+    }
 
 }
