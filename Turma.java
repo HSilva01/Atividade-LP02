aeee1051e269ad21f97a74d8d1a69c51e0451a00
Hérica <hericadias987@gmail.com>
2025-03-01 19:14:23 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -2,59 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Turma {
-
     private String identificacao;
     private Curso curso;
     private String semestre;
-    private final List<Professor> professores;
-    private final List<Estudante> estudantes;
-    private final List<Log> logs;
+    private List<Professor> professores = new ArrayList<>();
+    private List<Estudante> estudantes = new ArrayList<>();
+    private List<Log> logs = new ArrayList<>();
 
     public Turma(String identificacao, Curso curso, String semestre) {
         this.identificacao = identificacao;
         this.curso = curso;
         this.semestre = semestre;
-        this.professores = new ArrayList<>();
-        this.estudantes = new ArrayList<>();
-        this.logs = new ArrayList<>();
-    }
-
-    public String getIdentificacao() {
-        return identificacao;
-    }
-
-    public void setIdentificacao(String identificacao) {
-        this.identificacao = identificacao;
-    }
-
-    public Curso getCurso() {
-        return curso;
-    }
-
-    public void setCurso(Curso curso) {
-        this.curso = curso;
-    }
-
-    public String getSemestre() {
-        return semestre;
-    }
-
-    public void setSemestre(String semestre) {
-        this.semestre = semestre;
-    }
-
-    public List<Professor> getProfessores() {
-        return professores;
-    }
-
-    public void adicionarProfessor(Professor professor) {
-        if (professor != null) {
-            professores.add(professor);
-        }
-    }
-
-    public List<Estudante> getEstudantes() {
-        return estudantes;
     }
 
     public void adicionarEstudante(Estudante estudante) {
@@ -63,36 +21,55 @@ public class Turma {
         }
     }
 
-    public List<Log> getLogs() {
-        return logs;
-    }
-
-    public void adicionarLog(Log log) {
-        if (log != null) {
-            logs.add(log);
+    public void adicionarProfessor(Professor professor) {
+        if (professor != null) {
+            professores.add(professor);
         }
     }
 
-    public void alterarNotaEstudante(Professor coordenador, Estudante estudante, double novaNota) {
-        if (coordenador.isCoordenador()) {
-            System.out.println("Notas alteradas com sucesso para " + estudante.getNome());
+    public void alterarNotaEstudante(Professor coordenador, Estudante estudante, double novaNota1, double novaNota2, double novaNota3) {
+        if (coordenador.getCoordenador()) {
+            double[] notasAnteriores = { estudante.getNota().getNota1(), estudante.getNota().getNota2(), estudante.getNota().getNota3() };
+            estudante.getNota().setNota1(novaNota1);
+            estudante.getNota().setNota2(novaNota2);
+            estudante.getNota().setNota3(novaNota3);
+            logs.add(new Log(coordenador.getNome(), notasAnteriores, new double[]{novaNota1, novaNota2, novaNota3}));
+            System.out.println("Notas alteradas com sucesso!");
         } else {
             System.out.println("Somente coordenadores podem alterar notas.");
         }
     }
 
-    public void exibirDados() {
-        System.out.println("Turma: " + identificacao);
-        System.out.println("Curso: " + curso.getNome());
-        System.out.println("Semestre: " + semestre);
-        System.out.println("Professores:");
-        for (Professor professor : professores) {
-            professor.exibirDados();
-        }
-        System.out.println("Estudantes:");
+    public void exibirEstatisticas() {
+        int aprovados = 0, reprovados = 0, recuperacao = 0;
         for (Estudante estudante : estudantes) {
-            estudante.exibirDados();
+            double media = estudante.getNota().calcularMedia();
+            if (media >= 7) {
+                aprovados++;
+            } else if (media >= 2.5) {
+                recuperacao++;
+            } else {
+                reprovados++;
+            }
+        }
+        System.out.println("Aprovados: " + aprovados);
+        System.out.println("Em recuperação: " + recuperacao);
+        System.out.println("Reprovados: " + reprovados);
+    }
+
+    public void exibirListaRecuperacao() {
+        System.out.println("Estudantes em recuperação:");
+        for (Estudante estudante : estudantes) {
+            if (estudante.getNota().calcularMedia() >= 2.5 && estudante.getNota().calcularMedia() < 7) {
+                estudante.exibirDados();
+            }
+        }
+    }
+
+    public void exibirLogs() {
+        System.out.println("Histórico de alterações de notas:");
+        for (Log log : logs) {
+            log.exibirLog();
         }
     }
 }
-
