95cca810b047ccf3a9985ef2a3be8e67c01f8790
Hérica <hericadias987@gmail.com>
2025-03-01 12:24:26 -0300

    Update Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -1,34 +1,31 @@
-java
-package classes;
-
 public class Estudante extends Pessoa implements Printable {
-  private String matricula;
 
-  public Estudante() {}
+    private String matricula;
 
-  public Estudante(String nome, String cpf, String telefone, String endereco, String matricula, Double notaRecuperacao) {
-    super(nome, cpf, telefone, endereco);
-    this.matricula = matricula;
-  }
+    public Estudante(String nome, String cpf, String telefone, String endereco, String matricula, Double notaRecuperacao) {
+        super(nome, cpf, telefone, endereco);
+        this.matricula = matricula;
 
-  public String getMatricula() {
-    return this.matricula;
-  }
+    }
 
-  public void setMatricula(String matricula) {
-    this.matricula = matricula;
-  }
+    public String getMatricula() {
+        return this.matricula;
+    }
 
-  public void exibirDados() {
-    System.out.println("Nome: " + this.getNome());
-    System.out.println("CPF: " + this.getCpf());
-    System.out.println("Endereço: " + this.getEndereco());
-    System.out.println("Telefone: " + this.getTelefone());
-    System.out.println("Matrícula: " + this.matricula);
-  }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 
-  public void exibirHistorico() {
-    System.out.println("Histórico do estudante " + this.getNome() + ":");
-  }
+    @Override
+    public void exibirDados() {
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Endereço: " + this.getEndereco());
+        System.out.println("Telefone: " + this.getTelefone());
+        System.out.println("Matrícula: " + this.matricula);
+    }
+
+    public void exibirHistorico() {
+        System.out.println("Histórico do aluno " + this.getNome() + ":");
+    }
 }
-
