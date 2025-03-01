0e1b9532e590d81ea2cd5e5ac1d4620083a9411b
Hérica <hericadias987@gmail.com>
2025-03-01 15:42:32 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,11 +1,13 @@
+
 public class Professor extends Pessoa {
 
     private String siape;
-    private boolean coordenador = false;
+    private boolean coordenador;
 
     public Professor(String nome, String cpf, String telefone, String endereco, String siape) {
         super(nome, cpf, telefone, endereco);
         this.siape = siape;
+        this.coordenador = false; // Por padrão, o professor não é coordenador
     }
 
     public String getSiape() {
@@ -16,17 +18,18 @@ public class Professor extends Pessoa {
         this.siape = siape;
     }
 
-    public boolean getCoordenador() {
+    public boolean isCoordenador() {
         return coordenador;
     }
 
-    public void setCoodernador(boolean coordenador) {
+    public void setCoordenador(boolean coordenador) {
         this.coordenador = coordenador;
     }
 
     @Override
     public void exibirDados() {
+        super.exibirDados();
         System.out.println("SIAPE: " + siape);
+        System.out.println("Coordenador: " + (coordenador ? "Sim" : "Não"));
     }
-
 }
